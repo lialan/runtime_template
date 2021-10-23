d704a7e8705582ffd09ffdc48853ff5edd39befe
Alan Li <alan.li@me.com>
2021-10-23 02:03:07 +0000

    temp entry.

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+clang++-12 -G -D test.cpp -lfmt
+
--- test.cpp
+++ test.cpp
@@ -2,11 +2,7 @@
 #include <sstream>
 #include <fmt/core.h>
 
-int main(int argc, char *argv[]) {
-  //std::string t1(argv[1]);
-  //std::string t2(argv[2]);
-	std::string t1 = "int";
-	std::string t2 = "float";
+void gen_source_code(std::string t1, std::string t2, std::string file) {
 
   std::string template_function =
 "#include <iostream>\n"
@@ -21,15 +17,39 @@ int main(int argc, char *argv[]) {
   ss << template_function;
 
   // specialize template
-  ss << fmt::format("template<> void foo<{0}, {1}>({0} *a, {1} *b);", t1, t2) << std::endl;
+  //ss << fmt::format("template<> void foo<{0}, {1}>({0} *a, {1} *b);", t1, t2) << std::endl;
 
   // wrapper function:
-  ss << fmt::format("extern \"C\" foowrapper({0} *a, {1} *b)", t1, t2) << std::endl;
+  ss << fmt::format("extern \"C\" void foowrapper({0} *a, {1} *b)", t1, t2) << std::endl;
   ss << "{" << std::endl;
   ss << fmt::format("    foo<{0}, {1}>(a, b);", t1, t2) << std::endl;
   ss << "}" << std::endl;
 
   std::cout << ss.str() << std::endl;
+  
+  std::ofstream out(file);
+  out << ss.str();
+  out.close();
+}
+
+void gen_dylib(std::string infile, std::string outfile) {
+  std::system(fmt::format("clang++12 -shared -fPIC -o {0} {1}", infile, outfile));
+}
+
+int main(int argc, char *argv[]) {
+  std::string t1(argv[1]);
+  std::string t2(argv[2]);
+
+  std::string source_file = "/tmp/template.cc";
+  std::string dylib_file = "/tmp/templatelib.so";
+
+  gen_source_code(t1,t2, source_file);
+
+  gen_dylib(source_file, outfile);
+
+  load_dylib(outfile);
+
+  call_dylib("int", "float");
 
   return 0;
 }
